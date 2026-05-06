871b59973af41a421964f5ac33fae15aec0349ca
Nec3000 <nicoecurras@gmail.com>
2026-05-06 20:06:20 +0200

    Inicio parametro y clase Gasto

--- MercadoMonitor.java
+++ MercadoMonitor.java
@@ -1,19 +1,54 @@
 // Nunca cambia la declaracion del package!
 package cc.mercado;
 
+import es.upm.babel.cclib.Monitor;
+
+import java.util.HashMap;
+
 /**
  * Implementación del recurso compartido Carretera con Monitores
  */
 public class MercadoMonitor implements Mercado {
   // TODO: añadir atributos para representar el estado del recurso y
   // la gestión de la concurrencia (monitor y conditions)
+  Monitor mutex;
+
+  // atributos para representar el estado del recurso
+  private HashMap<Integer,Oferta> compras;
+  private HashMap<Integer,Oferta> ventas;
+  private int max;
+  private int min;
+  private int id_cont;
+
+  private static class Oferta {
+      int precio;
+      int ticks;
+      int dinero;
+      public Oferta(int precio, int ticks, int dinero) {
+            this.precio = precio;
+            this.ticks = ticks;
+            this.dinero = dinero;
+      }
+  }
 
   public MercadoMonitor() {
     // TODO: inicializar estado, monitor y conditions
+      compras = new HashMap<>();
+      ventas = new HashMap<>();
+      max = Integer.MAX_VALUE;
+      min = Integer.MIN_VALUE;//queremos emular el infinito del C-tad, es el menor numero posible dado por java, lo mismo con max
+      id_cont = 0;
+      mutex = new Monitor();
   }
 
   public int venta(int minPrecio, int tks) {
     // TODO: implementar venta
+      mutex.enter();
+      int resultado = id_cont;
+      id_cont++;
+
+
+
     return -1;
   }
 
@@ -39,3 +74,5 @@ public class MercadoMonitor implements Mercado {
     // TODO: implementar tick
   }
 }
+
+
